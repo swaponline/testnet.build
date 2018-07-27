616e511adeb67a440d56f2a518e08b4e9a7a05bb
nikdementev <nikita.pietrov.71@mail.ru>
2018-07-27 19:20:14 +0300

    change ETH contract

--- vendor.eeb61c.js
+++ vendor.eeb61c.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "22c3f7" + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "eeb61c" + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
