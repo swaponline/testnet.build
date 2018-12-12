83de2576790ccceaeb91f8623c8909d6e9766c99
BootWP <burdianov@gmail.com>
2018-12-12 06:17:21 +0200

    Update js

--- card2card/js/common.js
+++ card2card/js/common.js
@@ -1,31 +1,47 @@
 $(function () {
 	// tooltipe
 	$('[data-toggle="tooltip"]').tooltip({
-        html:true
+		html:true
 	});
 
-    //toggle mobile bar
-    $(".menu-toggle").on('click', function() {
-        $(this).toggleClass("on");
-        $('.menu-section').toggleClass("on");
-        $("nav ul").toggleClass('hidden');
+	//toggle mobile bar
+	$(".menu-toggle").on('click', function() {
+		$(this).toggleClass("on");
+		$('.menu-section').toggleClass("on");
+		$("nav ul").toggleClass('hidden');
 
-        $('.mobileBar').toggleClass('show');
-        $('body').toggleClass('showMobileBar');
-    });
+		$('.mobileBar').toggleClass('show');
+		$('body').toggleClass('showMobileBar');
+	});
 
-    // open video
-    $('#videoPreview').click(function(){
-        $('#player').fadeIn(100);
-        $("#player iframe")[0].src += "&autoplay=1";
-    });
-    $(document).on('click','#player-close',function(){
-        $('#player').fadeOut(100);
-        $("#player iframe").attr('src','https://www.youtube.com/embed/1G-U5Sh7pQk?rel=0&amp;showinfo=0');
-    });
+	// open video
+	$('#videoPreview').click(function(){
+		$('#player').fadeIn(100);
+		$("#player iframe")[0].src += "&autoplay=1";
+	});
+	$(document).on('click','#player-close',function(){
+		$('#player').fadeOut(100);
+		$("#player iframe").attr('src','https://www.youtube.com/embed/1G-U5Sh7pQk?rel=0&amp;showinfo=0');
+	});
 
 
-    // select
-    $('.giveAway').ddslick();
-    $('.areGetting').ddslick();
+	// select
+	$('#giveAway').ddslick();
+	$('#areGetting').ddslick();
+
+	// Exchange Reverse
+	$(document).on('click', '.btn-exchange-reverse', function(e){
+		e.preventDefault();
+		$('#giveAway').ddslick('destroy');
+		$('#areGetting').ddslick('destroy');
+		var giveAway = $('#giveAway');
+		var areGetting = $('#areGetting');
+		var giveAwayHtml = $('#giveAway').clone();
+		var areGettingHtml = $('#areGetting').clone();
+		giveAway.replaceWith(areGettingHtml);
+		areGetting.replaceWith(giveAwayHtml);
+		$('#giveAway').ddslick();
+		$('#areGetting').ddslick();
+	});
+
 });
